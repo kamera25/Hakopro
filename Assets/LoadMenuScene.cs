d6a38108a412066e0c7b149259e8a89c93788b6d
Takahiro Nakaoku <giraffe_rakugaki@yahoo.co.jp>
2014-10-08 11:33:44 +0900

    メニュー画面の追加

---
+++ Assets/LoadMenuScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMenuScene : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Invoke ("LoadMenu", 5F);
+	}
+	
+	void LoadMenu()
+	{
+		Application.LoadLevel("Menu");
+	}
+}
---
+++ Assets/MenuSceneLoder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSceneLoder : MonoBehaviour 
+{
+	public string scene;
+
+	public void LoadSceneButton()
+	{
+		if (scene == null) 
+		{
+			Debug.LogError("No set scene!!!");
+			return;
+		}
+		Application.LoadLevel (scene);
+	}
+}
