bae0b093d1c3c094c5e32d701442b8afe9a65be1
Takahiro Nakaoku <giraffe_rakugaki@yahoo.co.jp>
2015-02-15 11:28:17 +0900

    TopメニューSceneのリファクタリング

--- Assets/Componets/MenuSceneLoder.cs
+++ Assets/Componets/MenuSceneLoder.cs
@@ -4,22 +4,15 @@ using System.Collections;
 public class MenuSceneLoder : MonoBehaviour 
 {
 	public string scene;
-	private ScrollController scrollCtr;
-
-	void Start()
-	{
-		scrollCtr = GameObject.Find("Panel_b").GetComponent<ScrollController> ();
-	}
-
+	
 	public void LoadSceneButton()
 	{
-				if (scene == null) {
-						Debug.LogError ("No set scene!!!");
-						return;
-				}
+		if (scene == null) 
+		{
+			Debug.LogError ("No set scene!!!");
+			return;
+		}
 
-				if (scrollCtr.moveDist < 0.001F) {
-						Application.LoadLevel (scene);
-				}
+		Application.LoadLevel (scene);
 	}
 }
